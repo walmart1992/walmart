8c596e3b8f14446b618686a1005e8eea67f66caa
nethra92 <namanethra@gmail.com>
2022-07-23 18:46:08 +0530

    updated

--- DButils.java
+++ DButils.java
@@ -1,11 +1,15 @@
 
 
 hi guys this is for master branch
+<<<<<<< HEAD
 this line is added for merge command
 hi this is for development branch
 hi guys this is for master branch
 hi this is for development branch
 hi guys this is for master branch
 this line is added for merge command
+=======
+this line is added for merge command.....
+>>>>>>> c19413b (upadted)
 Hello Guys
 DB logic added

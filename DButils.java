951e54820f23470506cb93288424c44af8a75450
nethra92 <namanethra@gmail.com>
2022-07-23 18:46:08 +0530

    updated

--- DButils.java
+++ DButils.java
@@ -2,12 +2,16 @@
 
 hi guys this is for master branch
 <<<<<<< HEAD
+<<<<<<< HEAD
 this line is added for merge command
 hi this is for development branch
 hi guys this is for master branch
 hi this is for development branch
 hi guys this is for master branch
 this line is added for merge command
+=======
+this line is added for merge command.....
+>>>>>>> c19413b (upadted)
 =======
 this line is added for merge command.....
 >>>>>>> c19413b (upadted)
--- test.rb
+++ test.rb
@@ -1,1 +1,2 @@
 this line is added for updating
+hello
